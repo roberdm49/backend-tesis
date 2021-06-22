9af0eaf3cf313e2e2ee0d22f1106e79cd4ec946a
rdmarcos49 <roberto.dmarcos@hotmail.com>
2021-06-22 20:40:03 -0300

    redundant json() removed

--- src/index.js
+++ src/index.js
@@ -18,7 +18,6 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json({limit: '5mb'}))
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 app.set('key', config.key);
 
