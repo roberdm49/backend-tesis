5941f7f42b85a37c04ff7b9aaa691fc89626b1ca
rdmarcos49 <roberto.dmarcos@hotmail.com>
2021-06-23 23:42:27 -0300

    path fixed

--- src/api/routes/Login.js
+++ src/api/routes/Login.js
@@ -5,7 +5,7 @@ const usersFile = '../../mocks/users.json';
 const users = require(usersFile);
 
 
-const { createTokens } = require('../../config/tokenGenerator');
+const { createTokens } = require('../../config/createToken');
 
 router.post('/', (req, res) => {
   const { username, password } = {...req.body};
