8912b8960974106a682dc242c66a9780aeef8f1b
rdmarcos49 <roberto.dmarcos@hotmail.com>
2021-07-26 14:56:51 -0300

    comment about jwt

--- src/config/createToken.js
+++ src/config/createToken.js
@@ -2,7 +2,7 @@ const { sign } = require('jsonwebtoken');
 
 const createTokens = (user) => {
   const { id, username, name, lastname, avatar, role } = user;
-  const accessToken = sign({ id, username, name, lastname, avatar, role }, 'jwtsecretplschange');
+  const accessToken = sign({ id, username, name, lastname, avatar, role }, 'jwtsecretplschange'); //TODO: change this for an ENV variable
 
   return accessToken;
 };
