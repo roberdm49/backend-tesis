83ce31bbf381e14c778514693a450ae5eb087d13
rdmarcos49 <roberto.dmarcos@hotmail.com>
2021-08-12 11:33:05 -0300

    removed spaces

--- src/api/controllers/patients.js
+++ src/api/controllers/patients.js
@@ -32,20 +32,21 @@ patientsRouter.post('/', userExtractor, async (request, response, next) => {
     check.save().catch(err => next(err))
     patient.checks = patient.checks.concat(check._id)
     await patient.save().catch(err => next(err))
+
     return response.status(201).end()
   } else {
-    // TODO: CHEQUEAR
     const patientDB = new Patient({ dni, name, lastname, gender, birthDate, diabetesType })
     const savedPatient = await patientDB.save().catch(err => next(err))
-    // const savedPatient = await Patient.findOne({ dni }) // refactor this because is some redundant
     const check = new Check({
       checkDate,
       images,
       patient: savedPatient._id
     })
+
     const savedCheck = await check.save().catch(err => next(err))
     savedPatient.checks = savedPatient.checks.concat(savedCheck._id)
     await savedPatient.save().catch(err => next(err))
+
     return response.status(201).end()
   }
 })
@@ -54,7 +55,6 @@ patientsRouter.get('/:dni', userExtractor, async (request, response) => {
   const { dni } = request.params
   const patient = await Patient.findOne({ dni })
 
-  console.log(patient)
   return patient
     ? response.status(200).json({ patient })
     : response.status(400).json({ error: 'Dni not found' })
